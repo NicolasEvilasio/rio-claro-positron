38a60b73d8132ad609d44d996a305239e69d127b
nicolasevilasio <nicolas_evilasio@hotmail.com>
2024-12-19 01:04:38 -0300

    WIP: add an agent to selenium browser

--- pipeline/models/selenium_positron.py
+++ pipeline/models/selenium_positron.py
@@ -46,11 +46,16 @@ class Positron:
             options.add_argument('--no-sandbox')
             options.add_argument('--disable-dev-shm-usage')
             options.add_argument('--enable-automation')
+            options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
             
             # window size config
             options.set_preference("browser.window.width", 800)
             options.set_preference("browser.window.height", 600)
             
+            # Adicione argumentos para mascarar a automação
+            # options.add_argument('--disable-blink-features=AutomationControlled')
+            # options.add_experimental_option('excludeSwitches', ['enable-automation'])
+            # options.add_experimental_option('useAutomationExtension', False)
             
         print("Starting Firefox browser")
         cls.__driver = webdriver.Firefox(options=options)
